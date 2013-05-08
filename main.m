345e5b1f65f6dfc34e588a8edf5e234796c5724c
ShockingLee <shockingli@126.com>
2013-05-08 11:07:46 +0800

    第一次提交
    实现json文件解析，只是将下载的文件添加到工程中，还没有实际用到

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelpOrder
+//
+//  Created by 李小康 on 5/6/13.
+//  Copyright (c) 2013 李小康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "lxkAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([lxkAppDelegate class]));
+    }
+}
